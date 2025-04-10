a8e7568fade25fc09da1811995d21f3a67b809c6
penxer101 <139314203+penxer101@users.noreply.github.com>
2025-04-10 15:28:00 +0000

    commit

--- stress-test.js
+++ stress-test.js
@@ -1,18 +1,17 @@
 const http = require('http');
 
+const PORT = process.env.PORT || 3000;
+
 const server = http.createServer((req, res) => {
-  console.log(`${req.method} ${req.url}`);
-  
   if (req.url === '/test') {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ message: 'Hello from /test!' }));
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('You hit the /test route!');
   } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello from a raw Node.js server!');
   }
 });
 
-const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
